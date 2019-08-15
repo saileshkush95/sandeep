35a322069754343e6d373540795009c29a6333ea
Sandeep Kushwaha <saileshkush95@gmail.com>
2019-08-15 11:11:47 +0545

    change debug

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ PUBLIC_GOOGLE_RECAPTCHA_KEY = env('PUBLIC_GOOGLE_RECAPTCHA_KEY')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
